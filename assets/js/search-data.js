606beafeea6055215c66358195936c7b2204d43c
sterrrrrrr <157163043+sterrrrrrr@users.noreply.github.com>
2025-04-21 21:55:39 +0000

    Deploying to gh-pages from @ sterrrrrrr/sterrrrrrr.github.io@5473137b3459af278064b85aa18e201ed3520913 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Teaching Experiences",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
